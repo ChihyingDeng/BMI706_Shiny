58d501a00e8ed766fd0f06f36e3080196ba5f6ca
Chihying <liang1425@gmail.com>
2019-05-09 13:16:24 -0400

    update

--- main.R
+++ main.R
@@ -7,14 +7,25 @@ ui <- dashboardPage(
      dashboardHeader(title = "OSMI dataset"),
      dashboardSidebar(
           sidebarMenu(
-               menuItem("Question", tabName = "Question"),
-               menuItem("Living_country", tabName = "Living_country"),
-               menuItem("Work", tabName = "Work"),
-               menuItem("Disease", tabName = "Disease")
+               menuItem("Home", tabName = "Home", 
+                        icon = icon("home")),
+               menuItem("Question", tabName = "Question", 
+                        icon = icon("check")),
+               menuItem("Living_country", tabName = "Living_country",
+                        icon = icon("check")),
+               menuItem("Work", tabName = "Work",
+                        icon = icon("check")),
+               menuItem("Disease", tabName = "Disease",
+                        icon = icon("check")),
+               menuItem("Source code", icon = icon("file-code-o"), 
+                        href = "https://github.com/ChihyingDeng/BMI706_Shiny")
           )
      ),
      dashboardBody(
           tabItems(
+               tabItem(tabName = "Home",
+                       h2("Open Source Mental Illness (OSMI)"),
+                       p("2016 survey conducted on employees in tech community regarding mental health disorders")),
                tabItem(tabName = "Question",
                        h2(source("1_question.R"))),
                tabItem(tabName = "Living_country",
---
+++ t.R
@@ -0,0 +1,2 @@
+q_list<- read.csv('data/question_list.csv') 
+write.table(q_list)
